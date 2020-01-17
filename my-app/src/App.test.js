0dde1d036cda042ecc0895e4d6af2063ac0ad4d6
Kristin Parrish <kristinparrish@Kristins-MacBook-Pro.local>
2020-01-16 22:39:39 -0700

    tests added

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -2,15 +2,48 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('renders', () => {
+  test('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('renders strike', () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/strike/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test('renders ball', () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/ball/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test('renders out', () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/out/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("limits on balls outs and strikes", () => {
+  test('three strikes', () => {
+    const expected = 0;
+    const actual = strikeCount;
+    expect(actual).toBeLessThan(3);
+  });
 
+  test('three outs', () => {
+    const expected = 3;
+    const actual = outCount;
+    expect(actual).toBeLessThan(3);
+  });
+
+  test('four balls', () => {
+    const expected = 3;
+    const actual = ballCount;
+    expect(actual).toBeLessThan(4);
+  });
+});
