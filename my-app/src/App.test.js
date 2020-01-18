d28fa2eabf18deae8cfa27ebf11a74b51e5581d0
Kristin Parrish <kristinparrish@Kristins-MacBook-Pro.local>
2020-01-17 18:29:16 -0700

    tests updated

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App, {out, ball, strike}from './App'
 
 describe('renders', () => {
   test('renders without crashing', () => {
@@ -30,7 +30,7 @@ describe('renders', () => {
 
 describe("limits on balls outs and strikes", () => {
   test('three strikes', () => {
-    const expected = 0;
+    const expected = 3;
     const actual = strikeCount;
     expect(actual).toBeLessThan(3);
   });
@@ -42,8 +42,43 @@ describe("limits on balls outs and strikes", () => {
   });
 
   test('four balls', () => {
-    const expected = 3;
+    const expected = 4;
     const actual = ballCount;
     expect(actual).toBeLessThan(4);
+  });
+});
+
+describe("adding function of balls outs and strikes", () => {
+  test('out adds one to outs', () => {
+    let actual;
+    let expected;
+    actual = out(1);
+    expected = 2;
+    expect(actual).toBe(expected);
+    expect(actual).not.toBeGreaterThan(3);
+
+    expect(out(1)).toBe(2);
+  });
+
+  test('strike adds one to strikes', () => {
+    let actual;
+    let expected;
+    actual = strike(1);
+    expected = 2;
+    expect(actual).toBe(expected);
+    expect(actual).not.toBeGreaterThan(3);
+
+    expect(strike(1)).toBe(2);
+  });
+
+  test('ball adds one to balls', () => {
+    let actual;
+    let expected;
+    actual = ball(1);
+    expected = 2;
+    expect(actual).toBe(expected);
+    expect(actual).not.toBeGreaterThan(4);
+
+    expect(ball(1)).toBe(2);
   });
 });
